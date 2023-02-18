908bd83202dd0252002f640169066c89dbb47c37
VarunRd-124 <79012033+VarunRd-124@users.noreply.github.com>
2023-02-18 13:20:23 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "I'm PES2UG20CS568\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
